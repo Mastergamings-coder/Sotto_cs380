f021afa747cb21273cfcf15a83a60b90fdd78c85
Mastergamings-coder <philipsotti2004@gmail.com>
2026-04-29 20:17:28 +0800

    final Act

--- com/craftinginterpreters/tools/GenerateAst.java
+++ com/craftinginterpreters/tools/GenerateAst.java
@@ -27,28 +27,29 @@ public class GenerateAst {
             "Expression : Expr expression",
             "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
             "Print      : Expr expression",
-            "Var        : Token name, Expr initializer"
+            "Var        : Token name, Expr initializer",
+            "While      : Expr condition, Stmt body"
         ));
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
         String path = outputDir + "/" + baseName + ".java";
-        PrintWriter writer = new PrintWriter(path, "UTF-8");
-        writer.println("package com.craftinginterpreters.lox;");
-        writer.println();
-        writer.println("import java.util.List;");
-        writer.println();
-        writer.println("abstract class " + baseName + " {");
-        defineVisitor(writer, baseName, types);
-        for (String type : types) {
-            String className = type.split(":")[0].trim();
-            String fields = type.split(":")[1].trim();
-            defineType(writer, baseName, className, fields);
+        try (PrintWriter writer = new PrintWriter(path, "UTF-8")) {
+            writer.println("package com.craftinginterpreters.lox;");
+            writer.println();
+            writer.println("import java.util.List;");
+            writer.println();
+            writer.println("abstract class " + baseName + " {");
+            defineVisitor(writer, baseName, types);
+            for (String type : types) {
+                String className = type.split(":")[0].trim();
+                String fields = type.split(":")[1].trim();
+                defineType(writer, baseName, className, fields);
+            }
+            writer.println();
+            writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+            writer.println("}");
         }
-        writer.println();
-        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
-        writer.println("}");
-        writer.close();
     }
 
     private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
